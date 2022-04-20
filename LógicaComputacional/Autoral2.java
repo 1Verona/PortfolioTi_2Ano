8c1305a7f9ab6f71d2999ff725e83aec4353bd3a
Verona <102592096+1Verona@users.noreply.github.com>
2022-04-20 20:02:35 -0300

    Create Autoral2.java

---
+++ LógicaComputacional/Autoral2.java
@@ -0,0 +1,15 @@
+public class pg {
+    public static void main(String[] args) {
+        int inicial=1,
+            quociente=2,
+            gn=inicial,
+            valor_max=32;
+        
+        System.out.printf("Elementos da PG, de valor inicial %d e razão %d, menores que %d\n", inicial, quociente, valor_max );
+        while(gn<=valor_max){
+            System.out.println(gn);
+            gn *= quociente;
+        }
+    }
+
+}
